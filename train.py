fixed gradients + added tensorboard & parallel

train.py
@@ -1,22 +1,25 @@
-import pandas as pd
 from model import DiffPruning
-from transformers import (
-    BertForSequenceClassification,
-    BertTokenizer,
-)
+from transformers import BertForSequenceClassification, BertTokenizer
 import torch
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import TensorDataset, DataLoader, DistributedSampler
+from torch import distributed as distrib
 import logging
+
 
 logging.basicConfig(filename="log.txt", filemode='a', level=20)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 1
+DISTRIBUTED = True
+LOCAL_RANK = 0 if DISTRIBUTED else -1
+OPT_LEVEL = 'O1'
 
 
-def prepare_dataloader(data, labels, tokenizer):
+def prepare_dataloader(data, labels, tokenizer, batch_size):
     encodings = tokenizer(
         data,
         return_token_type_ids=True,
         max_length=None,
+        truncation=True,
         padding='max_length',
         return_tensors="pt"
     ).to(DEVICE)
@@ -26,18 +29,24 @@ def prepare_dataloader(data, labels, tokenizer):
         encodings["token_type_ids"],
         torch.tensor(labels, dtype=torch.long).to(DEVICE)
     )
-    dataloader = DataLoader(dataset,
-                            batch_size=1,
-                            shuffle=True)
-    return dataloader
+    distributed = distrib.is_available() and distrib.is_initialized()
+    sampler = None
+    if distributed:
+        sampler = DistributedSampler(data, num_replicas=distrib.get_world_size(), rank=distrib.get_rank())
+    return DataLoader(dataset, batch_size=batch_size, num_workers=1 if distributed else 0, sampler=sampler,
+                      shuffle=sampler is None)
 
 
 def main():
+    if DISTRIBUTED:
+        torch.cuda.set_device(0)
+        distrib.init_process_group("nccl")
+
     bert = BertForSequenceClassification.from_pretrained("bert-base-uncased")
     model = DiffPruning(bert, "bert", concrete_lower=-1.5, concrete_upper=1.5, total_layers=14,
-                        sparsity_penalty=0.000000125, weight_decay=0.0001, alpha_init=5.,
-                        lr_params=5e-5, lr_alpha=5e-5,
-                        warmup_steps=100, gradient_accumulation_steps=1, max_grad_norm=1, device=DEVICE)
+                        sparsity_penalty=1.25e-2, weight_decay=0.0001, alpha_init=5.,
+                        lr_params=5e-5, lr_alpha=1e-2, per_layer_alpha=False,
+                        warmup_steps=0, gradient_accumulation_steps=1, max_grad_norm=1, device=DEVICE)
 
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
@@ -49,12 +58,11 @@ def main():
                 "And another one just in case!"]
     val_labels = [0, 1]
 
-    train_dataloader = prepare_dataloader(train_data, train_labels, tokenizer)
-    val_dataloader = prepare_dataloader(val_data, val_labels, tokenizer)
+    train_dataloader = prepare_dataloader(train_data, train_labels, tokenizer, BATCH_SIZE)
+    val_dataloader = prepare_dataloader(val_data, val_labels, tokenizer, BATCH_SIZE)
 
-    model.train(train_dataloader, val_dataloader, 50)
+    model.train(train_dataloader, val_dataloader, 30, write=False)
 
 
 if __name__ == "__main__":
-    # args = parse_args()
     main()