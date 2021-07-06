fixed masking and loss

data.py
@@ -1,9 +1,12 @@
 import torch
 from torch.utils.data import TensorDataset, DistributedSampler, DataLoader
-import torch.distributed as dist
+import torch.distributed as distrib
+from transformers import PreTrainedTokenizer
+from typing import Optional, Tuple, Dict, Any
+from tags import START_TAG, END_TAG
 
 
-def tokens_to_dataloader(tokenized_examples, batch_size, task_name='squad', shuffle=True):
+def tokens_to_dataloader(tokenized_examples, batch_size, task_name='squad', shuffle=False):
     if task_name == 'squad':
         dataset = TensorDataset(
             torch.tensor(tokenized_examples["input_ids"], dtype=torch.long),
@@ -15,9 +18,87 @@ def tokens_to_dataloader(tokenized_examples, batch_size, task_name='squad', shuf
     else:
         dataset = tokenized_examples
 
-    distributed = dist.is_available() and dist.is_initialized()
+    distributed = distrib.is_available() and distrib.is_initialized()
     sampler = None
     if distributed:
-        sampler = DistributedSampler(dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank())
+        sampler = DistributedSampler(dataset, num_replicas=distrib.get_world_size(), rank=distrib.get_rank())
     return DataLoader(dataset, batch_size=batch_size, num_workers=1 if distributed else 0, sampler=sampler,
                       shuffle=sampler is None and shuffle)
+
+
+def insert_bounds(seqs: torch.Tensor, lens: torch.Tensor,
+                  start_code: Optional[int] = None, end_code: Optional[int] = None) \
+        -> Tuple[torch.Tensor, torch.Tensor]:
+    change = 0
+    start = 0
+    if start_code is not None:
+        change += 1
+        start = 1
+    if end_code is not None:
+        change += 1
+
+    new_shape = (seqs.shape[0], seqs.shape[1] + change)
+    new_seqs = torch.zeros(new_shape, dtype=seqs.dtype, device=seqs.device)
+    new_lens = lens.clone()
+
+    new_seqs[:, start:start + seqs.shape[1]] = seqs
+    new_lens[:] += change
+
+    if start_code is not None:
+        new_seqs[:, 0] = start_code
+    if end_code is not None:
+        new_seqs[range(lens.shape[0]), lens + change - 1] = end_code
+
+    return new_seqs, new_lens
+
+
+def create_mask(target: torch.Tensor, lens: torch.Tensor) -> torch.Tensor:
+    mask = torch.arange(target.size(1), dtype=lens.dtype, device=lens.device).repeat(target.size(0), 1) < lens.unsqueeze(1)
+    for i in range(len(target.size()) - 2):
+        mask = mask.unsqueeze(-1)
+    return mask.to(dtype=int)
+
+
+class SequenceBatch:
+    seqs: torch.Tensor
+    lens: torch.Tensor
+
+    def __init__(self, seqs: torch.Tensor, lens: torch.Tensor) -> None:
+        if torch.numel(lens) > 0:
+            max_len = lens.max().item()
+            seqs = seqs[:, :max_len]
+        self.seqs = seqs
+        self.lens = lens
+
+    def insert_bounds(self, start_code: Optional[int] = None, end_code: Optional[int] = None) -> 'SequenceBatch':
+        new_seqs, new_lens = insert_bounds(self.seqs, self.lens, start_code=start_code, end_code=end_code)
+        return SequenceBatch(new_seqs, new_lens)
+
+    def to(self, device: torch.device) -> 'SequenceBatch':
+        return SequenceBatch(self.seqs.to(device), self.lens.to(device))
+
+    def __len__(self):
+        return self.lens.shape[0]
+
+    @property
+    def device(self):
+        return self.seqs.device
+
+
+def parse_ner_batch(batch: Dict[str, Any], tokenizer: PreTrainedTokenizer,
+                    tags_dict: Dict[str, int]) -> Tuple[SequenceBatch, SequenceBatch]:
+    tensor = SequenceBatch(batch['input_ids'], batch['len'])
+    targets = SequenceBatch(batch['labels'], batch['len'])
+    inp_seq = tensor.insert_bounds(start_code=tokenizer.cls_token_id, end_code=tokenizer.sep_token_id)
+    out_seq = targets.insert_bounds(start_code=tags_dict[START_TAG], end_code=tags_dict[END_TAG])
+    return inp_seq, out_seq
+
+
+def get_ner_model_inputs(batch: Dict[str, Any], tokenizer: PreTrainedTokenizer,
+                         tags_vocab: Dict) -> Dict[str, Any]:
+    in_, out_ = parse_ner_batch(batch, tokenizer, tags_vocab)
+    return {
+        'input_ids': in_.seqs,
+        'labels': out_.seqs,
+        'attention_mask': create_mask(in_.seqs, in_.lens)
+    }

log.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from pathlib import Path
 from torch.utils.tensorboard import SummaryWriter
+from typing import NoReturn
 
-LOG_DIR = Path.cwd() / 'train_logs'
+import numpy as np
+
+LOG_DIR = Path.cwd() / 'runs'
 
 
 def prepare_log_dir(model_name: str, base_dir=LOG_DIR):
@@ -16,3 +19,56 @@ def setup_logging(model_name: str) -> SummaryWriter:
     log_dir = prepare_log_dir(model_name=model_name)
     writer = SummaryWriter(log_dir=str(log_dir))
     return writer
+
+
+def write_params(writer, args):
+    output = f'Model path {args.model_name_or_path}\n'
+    output += f'Task name {args.task_name}\n'
+    output += f'Epochs {args.num_train_epochs}\n'
+    output += f'W learning rate {args.lr_params}\n'
+    output += f'Alpha LR {args.lr_alpha}\n'
+    output += f'Sparsity penalty {args.sparsity_penalty}\n'
+    output += f'Alpha init {args.alpha_init}\n'
+    output += f'Weight decay {args.weight_decay}\n'
+    output += f'Batch size {args.batch_size}\n'
+    output += f'Per params alpha {args.per_params_alpha}\n'
+    output += f'Per layer alpha {args.per_layer_alpha}\n'
+    output += f'Concrete lower {args.concrete_lower}\n'
+    output += f'Concrete upper {args.concrete_upper}\n'
+    output += f'Start update steps {args.update_steps_start}'
+    writer.add_text('Parameters', output, args.update_steps_start)
+
+
+def log_start_training(logger, args, train_dataloader, t_total) -> NoReturn:
+    logger.info(f"***** Running training *****")
+    logger.info(f"  Model name = {args.model_name}")
+    logger.info(f"  Diff mode = {not args.no_diff}")
+    logger.info(f"  Num batches = {len(train_dataloader)}")
+    logger.info(f"  Num Epochs = {args.num_train_epochs}")
+    logger.info(f"  Batch size = {args.batch_size}")
+    logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
+    logger.info(f"  Total optimization steps = {t_total}")
+    logger.info(f"  Weight decay = {args.weight_decay}")
+    logger.info(f"  Concrete lower = {args.concrete_lower}")
+    logger.info(f"  Concrete upper = {args.concrete_upper}")
+    if not args.no_diff:
+        logger.info(f"  Params LR = {args.lr_params}, Alpha LR {args.lr_alpha}")
+        logger.info(f"  Sparsity penalty = {args.sparsity_penalty}")
+        logger.info(f"  Per params alpha = {args.per_params_alpha}")
+        logger.info(f"  Per layer alpha = {args.per_layer_alpha}")
+    else:
+        logger.info(f"  Params LR = {args.lr_params}")
+
+
+def log_epoch(logger, epoch: int, update_steps: int, train_losses: list,
+              val_losses: list, nonzero_params: list = None,
+              l0_penalties: list = None, no_diff=False) -> NoReturn:
+    string = f"Epoch {epoch + 1} | Update step {update_steps} average values:" + \
+             f"\n\ttrain loss = {np.array(train_losses).mean():.3f} "
+    if len(val_losses):
+        string += f"val loss = {np.array(val_losses).mean():.3f}"
+
+    if not no_diff:
+        string += f"\n\tnonzero params = {nonzero_params:,.0f}" + \
+                  f"\n\tL0 penalty = {np.array(l0_penalties).mean():,.3f}"
+    logger.info(string)

ner_utils.py
@@ -1,31 +1,23 @@
 from pathlib import Path
-from typing import List, NamedTuple, Dict, Any
+from typing import List, NamedTuple, Dict, Any, Optional, Tuple
 
-import numpy as np
 import torch
-from seqeval.metrics import precision_score, recall_score, f1_score
-from torch import nn
+import torch.nn.functional as F
+from torch import Tensor
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from transformers import PreTrainedTokenizer
 
+from data import get_ner_model_inputs
 from data import tokens_to_dataloader
-
-PAD_TAG = 'PAD'
-START_TAG = 'START'
-END_TAG = 'END'
-UNK_TAG = 'UNK'
-OTHER_TAG = 'O'
-
-UTIL_TAGS = [PAD_TAG, START_TAG, END_TAG, UNK_TAG, OTHER_TAG]
-ALT_ENTITIES = {'PROTEIN': 'GENE', 'CL': 'CELL_TYPE', 'CHEBI': 'CHEMICAL', 'GGP': 'GENE', 'SPECIES': 'TAXON',
-                'CELLLINE': 'CELL_LINE'}
-TAB = '\t'
+from tags import *
+from lm_model import loss as ner_loss
 
 
 class NerDatasetItem(NamedTuple):
     input_ids: List[int]
     labels: List[int]
+    len: int
 
 
 class RawNerItem(NamedTuple):
@@ -122,7 +114,7 @@ def encode_raw_ner_item(
         text_ids.extend([tokenizer.pad_token_id] * (cutoff - result_len))
         aligned_tags_ids.extend([pad_tag_id] * (cutoff - result_len))
 
-    return NerDatasetItem(input_ids=text_ids, labels=aligned_tags_ids)
+    return NerDatasetItem(input_ids=text_ids, labels=aligned_tags_ids, len=result_len)
 
 
 class NerDataset(Dataset):
@@ -136,7 +128,8 @@ class NerDataset(Dataset):
         item = self.items[index]
         return {
             'input_ids': torch.tensor(item.input_ids, dtype=torch.long),
-            'labels': torch.tensor(item.labels, dtype=torch.long)
+            'labels': torch.tensor(item.labels, dtype=torch.long),
+            'len': item.len
         }
 
     def __len__(self) -> int:
@@ -197,11 +190,12 @@ def load_dataset(path, tokenizer: PreTrainedTokenizer, tags_vocab,
                  cutoff: int = 200) -> NerDataset:
     raw_items = BioNerFileFormat.deserialize(path.read_text().splitlines())
     encoded_items: List[NerDatasetItem] = []
-    for item in tqdm(raw_items):
+    for i, item in enumerate(tqdm(raw_items)):
         encoded_item = encode_raw_ner_item(item, tokenizer=tokenizer, cutoff=cutoff,
                                            tags_vocab=tags_vocab)
         encoded_items.append(encoded_item)
     return NerDataset(encoded_items)
+
 
 def get_bc2gm_train_data(args, tokenizer, tags_vocab, return_val=True, return_train=True):
     train_dataloader, val_dataloader = None, None
@@ -214,38 +208,97 @@ def get_bc2gm_train_data(args, tokenizer, tags_vocab, return_val=True, return_tr
     return train_dataloader, val_dataloader
 
 
-def align_predictions(predictions: np.ndarray, label_ids: np.ndarray, label_map: dict):
-    pred = np.argmax(predictions, axis=2)
-    batch_size, seq_len = pred.shape
+def masked_softmax(inp: Tensor, mask: Tensor) -> Tensor:
+    masked_input = inp * mask.unsqueeze(2).float()
+    scores = F.softmax(masked_input, dim=2)
+    return scores
 
+
+def predict(logits: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+    scores = masked_softmax(logits, mask)
+    scores = scores * mask.unsqueeze(2).float()
+    path = torch.max(scores, 2)[1]
+    path = path * mask.long()
+    return path
+
+
+def align_predictions(logits, mask, label_ids, label_map: dict):
+    pred = predict(logits, mask).numpy()
+    batch_size, seq_len = pred.shape
     out_label_list = [[] for _ in range(batch_size)]
     preds_list = [[] for _ in range(batch_size)]
     for i in range(batch_size):
         for j in range(seq_len):
-            if label_ids[i, j] != nn.CrossEntropyLoss().ignore_index and label_map[label_ids[i, j]] != 'PAD':
-                out_label_list[i].append(label_map[label_ids[i][j]])
-                preds_list[i].append(label_map[pred[i][j]])
+            if label_map[label_ids[i, j]] != 'PAD' and label_map[label_ids[i, j]] != 'UNK' \
+                    and label_map[pred[i, j]] != 'PAD' and label_map[pred[i, j]] != 'UNK':
+                out_label_list[i].append(label_map[label_ids[i, j]])
+                preds_list[i].append(label_map[pred[i, j]])
     return preds_list, out_label_list
 
 
+def get_utils_tags_ids(vocab, util_tags: Optional[List[str]] = None) -> List[int]:
+    if util_tags is None:
+        util_tags = UTIL_TAGS
+    util_ids = list(sorted({id_ for tag, id_ in vocab.items() if tag in util_tags}))
+    assert max(util_ids) == len(util_ids) - 1  # check all util tags located at the start of vocab
+    return util_ids
+
+
+def update_confusion_matrix(conf_matrix: Tensor, actual: Tensor, predicted: Tensor, *, mask: Optional[Tensor] = None):
+    if mask is None:
+        mask = torch.ones_like(actual, dtype=torch.float32)
+
+    index = predicted * conf_matrix.size(1) + actual
+    conf_matrix.view(-1).index_add_(0, index, mask.float())
+
+
+def f1_score_micro_precision_recall(conf_matrix: Tensor, *, start: int = 0, end: Optional[int] = None) \
+        -> Tuple[Tensor, Tensor, Tensor]:
+    score = 0.
+    pr, p, r = 0., 0., 0.
+    for tag in range(start, conf_matrix.size(0) if end is None else end):
+        pr += conf_matrix[tag, tag].item()
+        p += torch.sum(conf_matrix[tag, :]).item()
+        r += torch.sum(conf_matrix[:, tag]).item()
+    try:
+        score = 2 * pr / (p + r)
+    except ZeroDivisionError:
+        pass
+    return torch.tensor(score), torch.tensor(pr / r), torch.tensor(pr / p)
+
+
 @torch.no_grad()
-def evaluate_ner_metrics(model, dataloader, label_map):
-    pr, rec, f1 = 0., 0., 0.
-    for batch in tqdm(dataloader, position=0, leave=True, desc="Evaluating metrics"):
-        if isinstance(batch, list):
-            outputs = model.forward([item.to(model.model.device) for item in batch])
-            labels = batch[-1]
-        else:
-            outputs = model.forward({key: value.to(model.model.device) for key, value in batch.items()})
-            labels = batch['labels']
-        pred, labels = align_predictions(outputs.logits.detach().cpu().numpy(), labels.cpu().numpy(), label_map)
+def evaluate_ner_metrics(model, dataloader, label_map, tokenizer):
+    tags_vocab = {value: key for key, value in label_map.items()}
+    tags_num = len(tags_vocab)
+    skip_tags = len(get_utils_tags_ids(tags_vocab))
+    conf_matrix = torch.zeros((tags_num, tags_num), dtype=torch.float)
+    total_loss = 0.
+    for batch in tqdm(dataloader, position=0, leave=True, desc="Validation"):
+        batch = get_ner_model_inputs(batch, tokenizer, tags_vocab)
+        outputs = model(**{key: value.to(model.device) for key, value in batch.items()})
+        total_loss += ner_loss(outputs.logits.cpu(), batch['labels'],
+                               batch['attention_mask'])  # output.loss.detach().item()
+        predictions = predict(outputs.logits.cpu(), batch['attention_mask'])
+        update_confusion_matrix(conf_matrix,
+                                actual=batch['labels'].reshape(-1), predicted=predictions.reshape(-1))
 
-        pr += precision_score(labels, pred, zero_division=0)
-        rec += recall_score(labels, pred, zero_division=0)
-        f1 += f1_score(labels, pred, zero_division=0)
-
-    return {
-        "precision": pr / len(dataloader),
-        "recall": rec / len(dataloader),
-        "f1": f1 / len(dataloader)
+    # pred, labels = align_predictions(outputs.logits.detach().cpu(), batch['attention_mask'].cpu(),
+    #                                  labels.cpu().numpy(), label_map)
+    # pr += precision_score(labels, pred, mode='strict', scheme=IOBES, zero_division=0)
+    # rec += recall_score(labels, pred, mode='strict', scheme=IOBES, zero_division=0)
+    # f1 += f1_score(labels, pred, mode='strict', scheme=IOBES, zero_division=0)
+    f1, pr, rec = f1_score_micro_precision_recall(conf_matrix, start=skip_tags)
+    return total_loss / len(dataloader), {
+        "precision": pr.item(),
+        "recall": rec.item(),
+        "f1": f1.item()
     }
+
+
+def create_mask(target: torch.Tensor, lens: torch.Tensor) -> torch.Tensor:
+    mask = torch.arange(target.size(1), dtype=lens.dtype, device=lens.device).repeat(target.size(0),
+                                                                                     1) < lens.unsqueeze(1)
+    for i in range(len(target.size()) - 2):
+        mask = mask.unsqueeze(-1)
+    return mask.to(dtype=torch.bool)

train.py
@@ -25,6 +25,7 @@ from model import DiffPruning
 from ner_utils import get_bc2gm_train_data, evaluate_ner_metrics, build_dict, UTIL_TAGS
 from squad_utils import get_train_data, get_validation_data, postprocess_qa_predictions
 
+
 MODEL_CLASSES = {'qa': {
     "bert": {"model": BertForQuestionAnswering,
              "config": BertConfig,
@@ -218,8 +219,11 @@ def eval_ner(args):
         else:
             diff_model.load(args.model_checkpoint, no_diff_load=True)
     set_seed(args)
-    metrics = evaluate_ner_metrics(diff_model, dataloader, label_map)
+
+    # torch.save(diff_model.model.state_dict(), "bert_state_dict.pt")
     nonzero, _ = diff_model._calculate_params(no_grads=True)
+    metrics = evaluate_ner_metrics(diff_model, dataloader, label_map, tokenizer)
+
     print(metrics)
     print(nonzero.item())
     print(nonzero.item() / 110_000_000 * 100)
@@ -261,7 +265,7 @@ def train(local_rank, args):
         args.model_name_or_path,
         from_tf=bool(".ckpt" in args.model_name_or_path),
         config=config
-    )
+    ).to("cuda" if torch.cuda.is_available() else "cpu")
 
     if args.task_name == 'squad':
         train_dataloader, val_dataloader = get_train_data(args=args,
@@ -288,6 +292,7 @@ def train(local_rank, args):
 
     set_seed(args)
     diff_model.train(local_rank=local_rank,
+                     args=args,
                      train_dataloader=train_dataloader,
                      val_dataloader=val_dataloader,
                      epochs=args.num_train_epochs,
@@ -298,7 +303,8 @@ def train(local_rank, args):
                      write=args.write,
                      update_steps_start=args.update_steps_start,
                      eval_func=eval_func,
-                     label_map=label_map)
+                     label_map=label_map,
+                     tokenizer=tokenizer)
 
 
 if __name__ == "__main__":